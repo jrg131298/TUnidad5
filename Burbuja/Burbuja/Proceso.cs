654a764efd89746ec1eb05e431d1135c06736346
jrg131298 <42877387+jrg131298@users.noreply.github.com>
2018-11-20 23:46:37 -0800

    5-1BubbleSort

---
+++ Burbuja/Burbuja/Proceso.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burbuja
+{
+    class Proceso
+    {
+        double[] Vector;
+        double Temporal;
+        public Proceso(double[] Vector)
+        {
+            this.Vector = Vector;
+        }
+        public void Impresion()
+        {
+            for (int Contador = 0; Contador < Vector.Length; Contador++)
+            {
+                Console.WriteLine(Vector[Contador]);
+            }
+        }
+        public void Acomodar()
+        {
+            for(int Contador1 = 1; Contador1 <= Vector.Length; Contador1++)
+            {
+                for(int Contador2 = 1; Contador2 < Vector.Length; Contador2++)
+                {
+                    if(Vector[Contador2 - 1] > Vector[Contador2])
+                    {
+                        Temporal = Vector[Contador2 - 1];
+                        Vector[Contador2 - 1] = Vector[Contador2];
+                        Vector[Contador2] = Temporal;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Burbuja/Burbuja/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burbuja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] Vector; //Donde se guardan los valores ingresados
+            int Cantidad, Auxiliar; //Cantidad = Cantidad de valores en el vector
+                                    //Auxiliar = Toma un valor auxiliar
+            bool Salir = false;
+            Proceso P;
+            do
+            {
+                try
+                {
+                    //Inicio del programa
+                    Console.WriteLine("Bubble Sort");
+                    Console.Write("Ingresa la cantidad de valores: ");
+                    Cantidad = Convert.ToInt32(Console.ReadLine());
+                    Vector = new double[Cantidad];
+                    //Captura de valores
+                    for (int Contador = 0; Contador < Cantidad; Contador++)
+                    {
+                        Console.Write("Ingresa un numero: ");
+                        Vector[Contador] = Convert.ToDouble(Console.ReadLine());
+                    }
+                    P = new Proceso(Vector);
+                    Console.Clear();
+                    //Impresion
+                    Console.WriteLine("Valores desordenados");
+                    P.Impresion();
+                    //Aplicacion del metodo de la burbuja
+                    P.Acomodar();
+                    //Impresion
+                    Console.WriteLine("\nValores ordenados");
+                    P.Impresion();
+                    Salir = true;
+                }
+                catch
+                {
+                    Console.WriteLine("A ocurrido un error.");
+                }
+                Console.WriteLine("Precione un boton para continuar."); Console.ReadKey();
+            } while (Salir == false); //Nos mantiene en la captura de datos
+        }
+    }
+}
